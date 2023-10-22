d806a638d5982d743efe2d76e1aca0894f0afe13
NARooshnavand <nasserakhoundi@gmail.com>
2023-10-22 03:45:50 -0700

    30-7-1402 first commit

--- session1/index.php
+++ session1/index.php
@@ -12,7 +12,14 @@ require_once('./functions.php');
     <?php
     require('./lessons.php');
     require('./profs.php');
-    dd($profs);
+    require('./relations.php');
+    // dd($profs_lessons);
+    foreach($profs_lessons as $key=>$value)
+    {
+        echo($key);
+        var_dump($value);
+        echo('<br>');
+    }
      
     ?>
     <body dir="rtl">
