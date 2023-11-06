73500793fae36645de04151247d58ad2e8d17360
NARooshnavand <nasserakhoundi@gmail.com>
2023-11-06 04:31:47 -0800

    15-8-1402 second commit

--- session1/index.php
+++ session1/index.php
@@ -2,26 +2,11 @@
 require_once('./functions.php');
 require_once('./MySQLDB.php');
 $db = new MySQLDB();
-// $db->createTable('profs', 'id int(11) PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256)');
-// $db->createTable('lessons', 'id int(11) PRIMARY KEY, title VARCHAR(256), vahed int(1), term int(1)');
-require_once('./lessons.php');
-foreach($lessons as $lesson)
+require_once('./profs.php');
+foreach($profs as $prof)
 {
-    $data = [
-        'title' => $lesson['title'],
-        'vahed' => $lesson['vahed'],
-        'term' => $lesson['term']
-    ];
-    $db->insert('lessons', $data);
-
+    $db->insert('profs',$prof);
 }
-dd($db);
-// $connection = connect_to_mysql();
-// $res = $connection->query("SHOW DATABASES;");
-// while ($row = mysqli_fetch_assoc($res)) {
-//     echo $row['Database'] . "\n";
-// }
-// dd($res);
 $number_of_hours = 4; 
 ?>
 <!DOCTYPE html>
